7376db4ed6a2829cfa77a8709a4bb60004a80b99
Henuks <159695878+Henuks@users.noreply.github.com>
2024-04-12 19:02:31 +0300

    Update KahootFlooder.ps1

--- KahootFlooder.ps1
+++ KahootFlooder.ps1
@@ -1,6 +1,6 @@
 #config
-$code="5538785"
-$amount=2
+$code="7097915"
+$amount=10
 $name="Test"
 
 #code
@@ -14,10 +14,10 @@ Start-Sleep -Milliseconds 1000
 $wshell.AppActivate((Get-Process Chrome).Where({ $_.MainWindowTitle }, 'First').Id)
 Start-Sleep -Milliseconds 1000
 $n=0
-$r=Get-Random
+$r=Get-Random -Maximum 100000
 while ($n -lt $amount) {
 $wshell.SendKeys('^tkahoot.it{enter}')
-Start-Sleep -Milliseconds 1000
+Start-Sleep -Milliseconds 1500
 $wshell.SendKeys('{tab}')
 Start-Sleep -Milliseconds 10
 $wshell.SendKeys('{tab}')
@@ -27,16 +27,16 @@ Start-Sleep -Milliseconds 10
 $wshell.SendKeys('{tab}')
 Start-Sleep -Milliseconds 10
 $wshell.SendKeys('{enter}')
-Start-Sleep -Milliseconds 800
+Start-Sleep -Milliseconds 700
 $wshell.SendKeys('{tab}')
 Start-Sleep -Milliseconds 10
 $wshell.SendKeys('{tab}')
 Start-Sleep -Milliseconds 10
-$wshell.SendKeys($name+" "+$r+'.'+$n)
+$wshell.SendKeys($name+" "+$r+$n)
 Start-Sleep -Milliseconds 10
 $wshell.SendKeys('{tab}')
 Start-Sleep -Milliseconds 10
 $wshell.SendKeys('{enter}')
+Start-Sleep -Milliseconds 20
 $n++
-Start-Sleep -Milliseconds 200
 }
